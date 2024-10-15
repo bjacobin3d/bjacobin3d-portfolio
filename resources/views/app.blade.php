fd6b7d012fac470219de8a81359f1acb44618d3c
Brent <b.jacobin3d@gmail.com>
2024-10-15 14:57:43 -0500

    add google fonts to csp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&family=Roboto:wght@400&display=swap" crossorigin="anonymous">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
